529460cac9a61e50de01debb0bcb66ed6b20020b
rodrigomrodovalho <rodrigomrodovalho@gmail.com>
2016-01-10 14:48:40 -0200

    Initial Commit

---
+++ src/main/java/br/com/rrodovalho/gcm_example_server/GcmExampleServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.rrodovalho.gcm_example_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcmExampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcmExampleServerApplication.class, args);
+	}
+}
